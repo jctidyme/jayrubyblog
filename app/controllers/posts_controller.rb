ecd1403a8630838f9741fd5a4909db0ef7af3795
Jay Carpenter <jay@tidyme.com.au>
2018-08-08 10:30:56 +1000

    database push

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+    def index
+        @content_first = 'This is some sample text for our awesome rubdy duby blog';
+        @content_second = 'This is some more sample text for our awesome ruby guby blog';
+    end
+    
+    def new
+        
+    end
+
+    def create
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+    def show
+        
+    end
+    
+    def destroy
+        
+    end
+
+end
