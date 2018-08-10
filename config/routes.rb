857c3396cae2378a627115258a1cf3ddb61a5cb6
Jay Carpenter <jay@tidyme.com.au>
2018-08-10 15:07:14 +1000

    commit to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'categories/edit'
   get 'categories/new'
   get 'categories/show'
-    get 'home/index'
+  get 'home/index'
         resources :posts
         resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
