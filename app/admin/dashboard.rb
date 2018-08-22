6e9c8bd6ff56639a6f1a5db803328cba409c87bc
Jay Carpenter <jay@tidyme.com.au>
2018-08-22 22:25:01 +1000

    Dashboard final updates.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,19 +4,27 @@ menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
     content title: proc{ I18n.t("active_admin.dashboard") } do
         div class: "blank_slate_container", id: "dashboard_default_message" do
-            span class: "blank_slate" do
-                span I18n.t("active_admin.dashboard_welcome.welcome")
-                small I18n.t("active_admin.dashboard_welcome.call_to_action")
-            end
-            table_for Post.order("id desc").limit(3) do 
-                column :id
-                column "Post Title", :title do |post|
-                    link_to post.title,[:admin,post]
+            section "Recent Posts" do
+                table_for Post.order("id desc").limit(5) do 
+                    column :id
+                    column "Post Title", :title do |post|
+                        link_to post.title,[:admin,post]
+                    end
+                    column :category
+                    column :created_at 
                 end
-                column :category
-                column :created_at 
+                strong (link_to "Show all posts",:admin_posts)
             end
-            strong (link_to "Show all posts",:admin_posts)
+            section "Categories" do
+                table_for Category.order("id desc").limit(5) do 
+                    column :id
+                    column "Category Title", :name do |category|
+                        link_to category.name,[:admin,category]
+                    end
+                    column :created_at 
+                end
+                strong (link_to "Show all categories",:admin_categories)                
+            end
         end
     end
 end
--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register Post do
 permit_params :title, :body, :category_id, :author_id
-
+	menu :label => "Blog Posts"
 end
