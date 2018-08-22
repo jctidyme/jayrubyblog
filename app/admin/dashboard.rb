956dafc148f771255eacae8b26ea6dfc7fdebe99
Jay Carpenter <jay@tidyme.com.au>
2018-08-22 22:00:35 +1000

    dashboard update

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,20 +1,22 @@
 ActiveAdmin.register_page "Dashboard" do
 
-    menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-        content title: proc{ I18n.t("active_admin.dashboard") } do
-            div class: "blank_slate_container", id: "dashboard_default_message" do
-                span class: "blank_slate" do
-                    span I18n.t("active_admin.dashboard_welcome.welcome")
-                    small I18n.t("active_admin.dashboard_welcome.call_to_action")
-                end
-                table_for "Recent Posts" do
-                    column do
-                    Post.last(5).map do |post|
-                        link_to(post.title, admin_post_path(post))                        
-                    end 
-                end
+    content title: proc{ I18n.t("active_admin.dashboard") } do
+        div class: "blank_slate_container", id: "dashboard_default_message" do
+            span class: "blank_slate" do
+                span I18n.t("active_admin.dashboard_welcome.welcome")
+                small I18n.t("active_admin.dashboard_welcome.call_to_action")
             end
+            table_for Post.order("id desc").limit(3) do 
+                column :id
+                column "Post Title", :title do |post|
+                    link_to post.title,[:admin,post]
+                end
+                column :category
+                column :created_at 
+            end
+            strong (link_to "Show all posts",:admin_posts)
         end
     end
 end
