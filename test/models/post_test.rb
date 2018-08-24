e10969d48939a29e94515bfedeebce68a41a1aaa
Jay Carpenter <jay@tidyme.com.au>
2018-08-24 14:11:43 +1000

    test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "If title is there" do 
+		post = Post.new(:title => "Some Title")
+		assert post.valid? 
+	end
 end
